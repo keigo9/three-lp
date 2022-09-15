14038140a1530a5bc77b5be124673418768e1db5
keigo9 <keigo.matsunari@gmail.com>
2022-09-15 23:02:44 +0900

    create mesh

--- main.js
+++ main.js
@@ -29,4 +29,28 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(size.width, size.height);
 renderer.setPixelRatio(window.devicePixelRatio);
 
+
+//オブジェクト作成
+const material = new THREE.MeshPhysicalMaterial({
+  color: "#3c94d7",
+  metalness: 0.86,
+  roughness: 0.37,
+  flatShading: true,
+})
+
+// ジオメトリ作成　mesh
+const mesh1 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.4, 16, 60), material);
+const mesh2 = new THREE.Mesh(new THREE.OctahedronGeometry(), material);
+const mesh3 = new THREE.Mesh(new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16), material);
+const mesh4 = new THREE.Mesh(new THREE.IcosahedronGeometry(), material);
+
+//回転用の配置
+mesh1.position.set(2, 0, 0);
+mesh2.position.set(-1, 0, 0);
+mesh3.position.set(2, 0, -6);
+mesh4.position.set(5, 0, 3);
+
+scene.add(mesh1, mesh2, mesh3, mesh4);
+
+
 renderer.render(scene, camera);
